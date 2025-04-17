e86a14eb578a1dfd7f738a8fc674c0ac00a920f7
Samir Singh <96865709+Facadedevil@users.noreply.github.com>
2025-04-17 17:46:10 +0800

    Create setting.sh

---
+++ setting.sh
@@ -0,0 +1,12 @@
+sudo tee /etc/docker/daemon.json <<EOF
+{
+  "default-runtime": "nvidia",
+  "runtimes": {
+    "nvidia": {
+      "path": "nvidia-container-runtime",
+      "runtimeArgs": []
+    }
+  }
+}
+EOF
+sudo systemctl restart docker
